4f9dc744af23c196641a428875e76dff875d3a6e
silvia712 <sielma712@gmail.com>
2020-11-26 19:21:41 +0100

    agenda

--- src/Agenda.java
+++ src/Agenda.java
@@ -2,40 +2,6 @@ import java.util.Scanner;
 
 public class Agenda {
 
-	public static int pintaMenu() {
-		int opc;
-		Scanner leer = new Scanner(System.in);
-
-		do {
-			System.out.println("1 Añadir, 2 Buscar, 3 Editar, 4 Borrar, 5 Ver todos, 6 Salir");
-			opc = leer.nextInt();
-
-			if (opc <= 0 || opc > 6) {
-				System.out.println("Error de opcion");
-			}
-		} while (opc <= 0 || opc > 6);
-
-		return opc;
-	}
-
-	public static void añadirContactos(String mContactos[][]) {
-
-		Scanner leer = new Scanner(System.in);
-
-		for (int i = 0; i < mContactos.length; i++) {
-
-			if (mContactos[i][0].equals("")) {
-				System.out.println("Introduce nombre");
-				mContactos[i][0] = leer.nextLine();
-				System.out.println("Introduce telefono");
-				mContactos[i][1] = leer.nextLine();
-				System.out.println("Contacto añadido");
-				break;
-			}
-		}
-
-	}
-
 	public static void inicializarMatriz(String mContactos[][]) {
 
 		for (int i = 0; i < mContactos.length; i++) {
@@ -43,40 +9,81 @@ public class Agenda {
 			mContactos[i][1] = "";
 		}
 	}
+		
+	public static int pintaMenu() {
+		
+		int opc;
+		Scanner leer = new Scanner(System.in);
 
-	public static void verTodos(String mContactos[][]) {
+		do {
+			System.out.println("    *** MENU ***");
+			System.out.println(" ");
+			System.out.println("1-Anadir   4-Borrar");
+			System.out.println("2-Buscar   5-Ver todos");
+			System.out.println("3-Editar   6-Salir");
+					
+			opc = leer.nextInt();
 
-		for (int i = 0; i < mContactos.length; i++) {
-			if (!mContactos[i][0].equalsIgnoreCase("")) {
-				System.out.println(mContactos[i][0] + " - " + mContactos[i][1]);
+			if (opc <= 0 || opc > 6) {
+				System.out.println("Error de opcion");
+				System.out.println(" ");
+			}
+		} while (opc <= 0 || opc > 6);
+
+		return opc;
+	}
+
+	public static void anadirContactos(String mContactos[][]) {
+
+		Scanner leer = new Scanner(System.in);
+
+		for (int i=0; i<mContactos.length; i++) {
+
+			if (mContactos[i][0].equals("")) {
+				System.out.println("Introduce nombre y telefono");
+				mContactos[i][0] = leer.nextLine();
+				mContactos[i][1] = leer.nextLine();
+				System.out.println("Contacto anadido");
+				System.out.println(" ");
+				break;
 			}
 		}
 	}
 
+	public static int buscarContacto(String mContactos[][]) {
+		//este metodo en realidad busca una posicion a mostrar
+		// Si no lo encuentro devuelvo -1 sino la posicion donde esta
+		//int pos = -1;
+
+		String busqueda;
+		Scanner leer = new Scanner(System.in);
+
+		//System.out.println("Escribe el criterio de busqueda");
+		busqueda = leer.nextLine();
+
+		for (int i = 0; i < mContactos.length; i++) {
+			if (mContactos[i][0].equalsIgnoreCase(busqueda) || mContactos[i][1].equalsIgnoreCase(busqueda)) {
+				return i; //posicion donde coincide la busqueda
+				}
+			}
+			// pos = i;
+			// break;
+			// return pos;
+			return -1;//-1 pq esta fuera del rango de una matriz
+		}
+	
 	public static void buscarOpcion(String mContactos[][]) {
 
 		int pos = buscarContacto(mContactos);
-		// posicion = buscarContacto(mContactos);
+		
 		if (pos == -1) {
 			System.out.println("No existe el contacto");
+			System.out.println(" ");
 		} else {
 			System.out.println(mContactos[pos][1]);
 		}
 	}
-
-	public static void borrarContacto(String mContactos[][]) {
-
-		// System.out.prinln("Escribe el contacto a borrar");
-		int pos = buscarContacto(mContactos);
-
-		if (pos == -1) {
-			System.out.println("No existe el contacto");
-		} else {
-			mContactos[pos][0] = "";
-			mContactos[pos][1] = "";
-		}
-	}
-
+	
 	public static void editarContacto(String mContactos[][]) {
 
 		String nuevoNombre, nuevoTelefono;
@@ -86,38 +93,40 @@ public class Agenda {
 
 		if (pos == -1) {
 			System.out.println("No existe el contacto");
-
+			System.out.println(" ");
 		} else {
-			//System.out.println("Escribe el nuevo nombre y telefono");
+			System.out.println("Escribe el nuevo nombre y telefono");
 			nuevoNombre = leer.nextLine();
 			nuevoTelefono = leer.nextLine();
 			mContactos[pos][0] = nuevoNombre;
 			mContactos[pos][1] = nuevoTelefono;
+			System.out.println("Cambios guardados");
+			System.out.println(" ");
 		}
 	}
+	
+	public static void borrarContacto(String mContactos[][]) {
 
-	// Si no lo encuentro devuelvo -1 sino la posicion donde esta
-	public static int buscarContacto(String mContactos[][]) {
-		// int pos = -1;
+		int pos = buscarContacto(mContactos);
 
-		String busqueda;
-		Scanner leer = new Scanner(System.in);
-
-		//System.out.println("Escribe el criterio de busqueda");
-		busqueda = leer.nextLine();
+		if (pos == -1) {
+			System.out.println("No existe el contacto");
+			System.out.println(" ");
+		} else {
+			mContactos[pos][0] = "";
+			mContactos[pos][1] = "";
+			System.out.println("Contacto borrado");
+			System.out.println(" ");
+		}
+	}
+	
+	public static void verTodos(String mContactos[][]) {
 
 		for (int i = 0; i < mContactos.length; i++) {
-			// for (int j=0;j<mContactos[j].length;j++){
-			if (mContactos[i][0].equalsIgnoreCase(busqueda) || mContactos[i][1].equalsIgnoreCase(busqueda)) {
-				// if (mContactos[i][j]).equalsIgnoreCase(busqueda){
-				return i;
+			if (!mContactos[i][0].equalsIgnoreCase("")) {
+				System.out.println(mContactos[i][0] + " - " + mContactos[i][1]);
 			}
 		}
-		// pos = i;
-		// break;
-		// return pos;
-		return -1;
-
 	}
 
 	public static void main(String[] args) {
@@ -132,7 +141,10 @@ public class Agenda {
 
 		inicializarMatriz(mContactos);
 		
+		System.out.println(" ");
 		System.out.println("AGENDA DE TELEFONOS (pulsa la opcion que desees)");
+		System.out.println(" ");
+		
 
 		do {
 			
@@ -140,28 +152,32 @@ public class Agenda {
 
 			switch (opcion) {
 			case 1:
-				System.out.println("1 - Añadir contacto ");
-				añadirContactos(mContactos);
+				System.out.println("1 - A�ADIR");
+				anadirContactos(mContactos);
 				break;
 			case 2:
-				System.out.println("2 - Buscar contacto: Escribe el nombre a buscar ");
+				System.out.println("2 - BUSCAR");
+				System.out.println("Escribe el nombre del contacto a buscar");
 				buscarOpcion(mContactos);
 				break;
 			case 3:
-				System.out.println("3 - Editar contacto: Escribe el nuevo nombre y telefono ");
-				editarContacto(mContactos);
+				System.out.println("3 - EDITAR");
+				System.out.println("Escribe el nombre del contacto a editar");	
+				editarContacto(mContactos);						
 				break;
 			case 4:
-				System.out.println("4 - Borrar contacto: escribe el contacto a borrar");
+				System.out.println("4 - BORRAR");
+				System.out.println("Escribe el nombre del contacto a borrar");			
 				borrarContacto(mContactos);
-				System.out.println("Contacto borrado");
+				
 				break;
 			case 5:
-				System.out.println("5 - Ver todos");
+				System.out.println("5 - VER TODOS");
 				verTodos(mContactos);
+				System.out.println(" ");
 				break;
 			case 6:
-				System.out.println("Hasta luego !");
+				System.out.println("Hasta luego!");
 				break;
 			}
 
